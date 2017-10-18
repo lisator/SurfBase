36a2667a207887c5b86b0b911c0c273a32dd6164
BuildTools <unconfigured@null.spigotmc.org>
2017-10-18 21:25:09 +0200

    Added diagram

---
+++ SurfBase/SurfBase/SurfBaseLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SurfBase
+{
+    public class SurfBaseLogic
+    {
+
+        public SurfBaseLogic()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
